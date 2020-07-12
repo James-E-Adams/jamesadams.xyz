69db6be76415d9e14d6d1be0919e8617e22c392e
James Adams <james.adams@ferocia.com.au>
2020-07-12 17:58:10 +1000

    fix

--- src/pages/words.js
+++ src/pages/words.js
@@ -3,7 +3,7 @@
 import * as React from "react"
 
 import { graphql, Link } from "gatsby"
-import StandardTemplate from "src/Templates/Standard"
+import StandardTemplate from "src/templates/Standard"
 
 type Props = {|
   +className?: string,
