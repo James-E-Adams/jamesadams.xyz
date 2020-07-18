341c7c826315c016f16deb796eb271496295aa73
James Adams <james.adams@ferocia.com.au>
2020-07-18 14:24:51 +1000

    add post footer

--- src/templates/post/index.js
+++ src/templates/post/index.js
@@ -14,11 +14,17 @@ function Post({ data, className }: Props) {
   const { frontmatter, html } = markdownRemark
   return (
     <StandardTemplate route="/words">
-      <div className="flex justify-center mt-20">
+      <div className="flex justify-center mt-16">
         <div className="w-4/5">
           <h2 className="text-4xl font-bold mb-2">{frontmatter.title}</h2>
           <h1>{frontmatter.date}</h1>
           <div className="mt-6" dangerouslySetInnerHTML={{ __html: html }} />
+          <hr />
+          <div className="mb-10">
+            Thanks for reading! Feel free to drop me a line at{" "}
+            <a href="mailto:hello@jamesadams.xyz"> hello@jamesadams.xyz </a> and
+            let me know your thoughts!
+          </div>
         </div>
       </div>
     </StandardTemplate>
